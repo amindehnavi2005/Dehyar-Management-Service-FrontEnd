02f08aa698afe605d3eb75c03275b0c02b387158
amin dehnavi <amin1384dehnavi@gmail.com>
2024-09-26 13:53:48 +0330

    refactor MachineBasicInformation

--- src/views/dehyari/form/machinery/StepsForm/MachineBasicInformation.jsx
+++ src/views/dehyari/form/machinery/StepsForm/MachineBasicInformation.jsx
@@ -1,6 +1,8 @@
 import DividerSimple from '@/components/common/Divider/DividerSimple';
-import { Box, Button, Grid, TextField } from '@mui/material';
-import React from 'react'
+import { getMachineBasicInformation, getMachineInformation } from '@/Services/Machine';
+import api from '@/utils/axiosInstance';
+import { Autocomplete, Box, Button, Grid, TextField } from '@mui/material';
+import React, { useEffect, useState } from 'react'
 import { Controller, useFormContext } from 'react-hook-form';
 
 const persianToEnglishDigits = (str) => {
@@ -9,57 +11,63 @@ const persianToEnglishDigits = (str) => {
     return str.replace(/[۰-۹]/g, (char) => englishDigits[persianDigits.indexOf(char)]);
 };
 
-const MachineBasicInformation = ({ setData, setStep }) => {
+const MachineBasicInformation = ({ setData }) => {
 
-    const { control, handleSubmit, formState: { errors } } = useFormContext();
+    const { control, formState: { errors } } = useFormContext();
 
-    const onSubmit = (data) => {
-        setStep(1);
+    const [basicInformation, setBasicInformation] = useState([]);
+
+    const fetchMachineBasicInformation = async () => {
+        await api.get(getMachineBasicInformation(), { requiresAuth: true })
+            .then((response) => {
+                setBasicInformation(response.data.data)
+                console.log(response.data);
+                console.log("Response => ", response.data.data);
+            })
     }
 
-    const renderTextField = (name, label) => (
-        <Controller
-            name={name}
-            control={control}
-            rules={{ required: true }}
-            render={({ field: { value, onChange } }) => (
-                <TextField
-                    InputProps={
-                        { style: { height: 45 }, inputProps: { style: { textAlign: 'center' } } }
-                    }
-                    label={label}
-                    value={value}
-                    onChange={(e) => {
-                        const value = persianToEnglishDigits(e.target.value);
-                        setData(prevValues => ({ ...prevValues, [name]: value }));
-                        onChange(value);
-                    }}
-                    fullWidth
-                    error={errors[name]}
-                    helperText={errors?.[name]?.message}
-                />
-            )}
-        />
-    );
+    useEffect(() => {
+        fetchMachineBasicInformation();
+    }, []);
 
     return (
-        <Grid container spacing={2} mt={1}>
-            <Grid item xs={12} mb={5}>
-                <DividerSimple title={'اطلاعات مورد نظر خودتان را انتخاب کنید'} />
-            </Grid>
-            <form className='w-full' onSubmit={handleSubmit(onSubmit)}>
-                <Grid container gap={5}>
-                    <div className='grid md:grid-cols-3 w-full gap-5'>
-                        {renderTextField('category', 'دسته بندی', 'دسته بندی الزامی است')}
-                        {renderTextField('machine_type', 'نوع', 'نوع الزامی است')}
-                        {renderTextField('machine_title', 'عنوان ماشین آلات', 'عنوان ماشین آلات الزامی است')}
-                    </div>
-                </Grid>
-                <Box display={'flex'} mt={2} gap={5} justifyContent={'end'} >
-                    <Button variant='contained' color='primary' type='submit' >بعدی</Button>
-                </Box>
-            </form>
-        </Grid>
+        <Controller
+            name='machine_basic_id'
+            control={control}
+            defaultValue={null}
+            render={({ field }) => {
+                const selectedOption = basicInformation.find(
+                    option => option.id === field.value?.id
+                );
+
+                return (
+                    <Autocomplete
+                        {...field}
+                        options={basicInformation}
+                        getOptionLabel={(option) => `${option.category} - ${option.type} - ${option.title}`}
+                        isOptionEqualToValue={(option, value) =>
+                            option.id === value?.id
+                        }
+                        onChange={(event, newValue) => {
+                            setData(prevValues => ({ ...prevValues, machine_basic_id: newValue.id }));
+                            field.onChange(newValue && newValue.id || null);
+                            console.log("New Value => ", newValue.id);
+                        }}
+                        value={selectedOption}
+                        renderInput={(params) => (
+                            <TextField
+                                autoComplete="off"
+                                {...params}
+                                label="اطلاعات پایه (دسته بندی - نوع - عنوان)"
+                                size="small"
+                                fullWidth
+                                error={errors.machine_basic_id}
+                            />
+                        )}
+                    />
+                );
+            }}
+        />
     )
 }
 
