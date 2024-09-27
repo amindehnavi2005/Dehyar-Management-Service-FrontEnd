cd1c458616fe1f774f012c59048dcd0d575e6698
amin dehnavi <amin1384dehnavi@gmail.com>
2024-09-27 13:53:47 +0330

    add typePlates to PlateCreator.jsx

--- src/views/dehyari/form/machinery/StepsForm/PlateCreator.jsx
+++ src/views/dehyari/form/machinery/StepsForm/PlateCreator.jsx
@@ -1,6 +1,7 @@
 import { FormControl, InputLabel, MenuItem, Select, TextField } from "@mui/material";
 import { React, useState } from "react";
 import { Controller, useFormContext } from "react-hook-form";
+import typePlates from "@data/typePlates"
 
 const persianToEnglishDigits = (str) => {
     const persianDigits = "۰۱۲۳۴۵۶۷۸۹";
@@ -11,12 +12,6 @@ const persianToEnglishDigits = (str) => {
 const PlateCreator = ({ setData }) => {
     const { control, handleSubmit, formState: { errors } } = useFormContext();
 
-    const typePlates = [
-        { value: 0, label: 'ش' },
-        { value: 1, label: 'د' },
-        { value: 2, label: 'ع' },
-    ]
-
 
     const renderTextField = (name, count) => (
         <Controller
@@ -25,16 +20,18 @@ const PlateCreator = ({ setData }) => {
             rules={{ required: true }}
             render={({ field: { value, onChange } }) => (
                 <TextField
+                    autoComplete="off"
                     variant="outlined"
                     placeholder={count == 2 ? "--" : "---"}
                     InputProps={
                         {
-                            style: { height: 45, width: count == 2 ? 55 : 75 }, inputProps: {
+                            style: { height: 45 }, inputProps: {
                                 maxLength: count == 2 ? 2 : 3,
                                 style: { textAlign: 'center', border: 'none', fontWeight: 'bold' }
                             }
                         }
                     }
+                    fullWidth
                     value={value}
                     onChange={(e) => {
                         const value = persianToEnglishDigits(e.target.value);
@@ -46,10 +43,12 @@ const PlateCreator = ({ setData }) => {
                             '& fieldset': {
                                 border: 'none', // حذف حاشیه
                             },
+                            '& input': {
+                                color: errors[name] ? 'red' : 'inherit', // رنگ متن را به قرمز تغییر دهید
+                            },
                         },
                     }}
                     error={errors[name]}
-                    helperText={errors?.[name]?.message}
                 />
             )}
         />
@@ -65,6 +64,7 @@ const PlateCreator = ({ setData }) => {
                     onChange={e => {
                         const newValue = e.target.value;
                         setTimeout(() => {
+                            console.log("Options => ", option[newValue]);
                             setData(prevValues => ({ ...prevValues, [name]: newValue }));
                             onChange(newValue)
                             console.log("Data =>", newValue);
@@ -75,59 +75,27 @@ const PlateCreator = ({ setData }) => {
                     error={errors[name]}
                     sx={{
                         border: 'none', // حذف حاشیه
-                            padding: '10px', // تنظیم padding
-                            '& .MuiSelect-select': {
-                                padding: '10px', // تنظیم padding داخلی
-                            },
-                            '& .MuiOutlinedInput-notchedOutline': {
-                                border: 'none', // حذف حاشیه
-                            },
-                            '&:before, &:after': {
-                                border: 'none', // حذف حاشیه قبل و بعد
-                            },
+                        padding: '10px', // تنظیم padding
+                        '& .MuiSelect-select': {
+                            padding: '10px', // تنظیم padding داخلی
+                        },
+                        '& .MuiOutlinedInput-notchedOutline': {
+                            border: 'none', // حذف حاشیه
+                        },
+                        '&:before, &:after': {
+                            border: 'none', // حذف حاشیه قبل و بعد
+                        },
                         height: 45,
+                        width: "10%"
                     }}
                 >
-                    {Object.entries(option.map(({ value, label }) => (
-                        <MenuItem key={value} value={value}>{label}</MenuItem>
-                    )))}
+                    {option.map(item => (
+                        <MenuItem key={item.value} value={item.value}>{item.label}</MenuItem>
+                    ))}
                 </Select>
             )}
         />
     )
-
-    // const renderSelect = (name, option) => (
-    //     <Controller
-    //         name={name}
-    //         control={control}
-    //         render={({ field: { value, onChange } }) => (
-    //             <Select
-    //                 value={value}
-    //                 onChange={e => {
-    //                     const newValue = e.target.value;
-    //                     setTimeout(() => {
-    //                         setData(prevValues => ({ ...prevValues, [name]: newValue }));
-    //                         onChange(newValue)
-    //                         console.log("Data =>", newValue);
-    //                     }, 0);
-    //                 }}
-    //                 error={errors[name]}
-    //                 sx={{
-    //                     '& .MuiOutlinedInput-root': {
-    //                         '& fieldset': {
-    //                             border: 'none', // حذف حاشیه
-    //                         },
-    //                     },
-    //                     height: 45,
-    //                 }}
-    //             >
-    //                 {Object.entries(option.map(({ value, label }) => (
-    //                     <MenuItem key={value} value={value}>{label}</MenuItem>
-    //                 )))}
-    //             </Select>
-    //         )}
-    //     />
-    // );
 
     return (
         <>
@@ -139,10 +107,10 @@ const PlateCreator = ({ setData }) => {
                     {renderSelect('plate_category_letter', typePlates)}
                     {renderTextField("plate_registration_number", 2)}
                 </div>
-                <div className="bg-blue-900 px-2 py-1 text-end rounded-l-sm">
-                    <div className="font-medium text-white text-sm">
-                        <p>.I.R</p>
-                        IRAN
+                <div className="flex items-center bg-blue-900 px-2 py-1 text-end rounded-l-sm">
+                    <div className="font-medium text-center text-white text-xs">
+                        <img className="h-3" src="/images/logos/iran-flag.jpg" alt="پرچم ایران" />
+                        <p>IRAN</p>
                     </div>
                 </div>
             </div>
