fa76e10753421b9726b315e8be424bbdd0b39878
amin dehnavi <amin1384dehnavi@gmail.com>
2024-12-21 18:44:52 +0330

    rename GradingVillageForRSD

---
+++ src/app/(dashboard)/dehyari/rsd/page.jsx
@@ -0,0 +1,9 @@
+import RSDTable from "@/views/dehyari/rural-services-development-manager/table/RSDTable";
+import React from "react";
+
+// مدیریت توسعه خدمات روستایی
+const page = () => {
+  return <RSDTable />;
+};
+
+export default page;
---
+++ src/app/(dashboard)/dehyari/rsd/upgrade-village-rank/page.jsx
@@ -0,0 +1,8 @@
+import GradingVillageForRSD from "@/views/dehyari/rural-services-development-manager/upgrade-village-rank/GradingVillageForRSD";
+import React from "react";
+
+const page = () => {
+  return <GradingVillageForRSD />;
+};
+
+export default page;
