59b09245b1cfde0adb7b36ccb7b10a88cc7c0e4e
amin dehnavi <amin1384dehnavi@gmail.com>
2024-12-22 07:17:37 +0330

    update workflow titles

--- src/views/dehyari/form/workflow/ReviewDecree.jsx
+++ src/views/dehyari/form/workflow/ReviewDecree.jsx
@@ -15,6 +15,7 @@ const ReviewDecree = ({
   handleDescriptionChange,
   renderRejectOptions,
   readOnly,
+  workflowType = "حکم کارگزینی",
 }) => {
   const handleDownloadPdf = () => {
     downloadHumanResourcePdf(
@@ -22,52 +23,106 @@ const ReviewDecree = ({
       details.human_contract_id
     );
   };
+  console.log("WorkFlowType => ", details);
+
+  const renderFields = () => {
+    if (workflowType === "حکم کارگزینی") {
+      return (
+        <>
+          <UserInfoItem
+            icon="ri-government-line"
+            label="پست سازمانی"
+            value={details ? details?.job_type : "نامشخص"}
+          />
+          <UserInfoItem
+            icon="ri-community-line"
+            label="دهیاری"
+            value={details ? details?.village?.approved_name : "نامشخص"}
+          />
+          <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
+            <UserInfoItem
+              icon="ri-file-line"
+              label="نوع قرارداد"
+              value={
+                details ? `${details?.contract_type} روز کارکرد` : "نامشخص"
+              }
+            />
+            {details && (
+              <Chip
+                icon={<ArticleIcon />}
+                label="حکم کارگزینی"
+                color="primary"
+                onClick={handleDownloadPdf}
+                sx={{ cursor: "pointer" }}
+                size="small"
+              />
+            )}
+          </Box>
+          <UserInfoItem
+            icon="ri-calendar-line"
+            label="تاریخ شروع قرارداد"
+            value={
+              details ? convertUnixToJalali(details.contract_end) : "نامشخص"
+            }
+          />
+          <UserInfoItem
+            icon="ri-calendar-line"
+            label="تاریخ اجرای قرارداد"
+            value={
+              details ? convertUnixToJalali(details.execute_start) : "نامشخص"
+            }
+          />
+          <UserInfoItem
+            icon="ri-wallet-2-line"
+            label="مبلغ حکم کارگزینی"
+            value="-"
+          />
+        </>
+      );
+    } else if (workflowType === "ارتقاء درجه") {
+      return (
+        <>
+          <UserInfoItem
+            icon="ri-map-pin-line"
+            label="استان"
+            value={details ? details.geo_state : "نامشخص"}
+          />
+          <UserInfoItem
+            icon="ri-map-pin-2-line"
+            label="شهرستان"
+            value={details ? details.geo_city : "نامشخص"}
+          />
+          <UserInfoItem
+            icon="ri-building-line"
+            label="بخش"
+            value={details ? details.geo_region : "نامشخص"}
+          />
+          <UserInfoItem
+            icon="ri-home-2-line"
+            label="دهستان"
+            value={details ? details.geo_village : "نامشخص"}
+          />
+          <UserInfoItem
+            icon="ri-arrow-down-s-line"
+            label="نوع درخواست"
+            value={
+              details ? (
+                <span className="text-xl">
+                  {details.new_grade} → {details.current_grade}
+                </span>
+              ) : (
+                "نامشخص"
+              )
+            }
+          />
+        </>
+      );
+    }
+  };
 
   return (
     <Box className={"flex flex-col gap-3"}>
-      <UserInfoItem
-        icon="ri-government-line"
-        label="پست سازمانی"
-        value={details ? details.job_type : "نامشخص"}
-      />
-      <UserInfoItem
-        icon="ri-community-line"
-        label="دهیاری"
-        value={details ? details.village.approved_name : "نامشخص"}
-      />
-      <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
-        <UserInfoItem
-          icon="ri-file-line"
-          label="نوع قرارداد"
-          value={details ? `${details.contract_type} روز کارکرد` : "نامشخص"}
-        />
-        {details && (
-          <Chip
-            icon={<ArticleIcon />}
-            label="حکم کارگزینی"
-            color="primary"
-            onClick={handleDownloadPdf}
-            sx={{ cursor: "pointer" }}
-            size="small"
-          />
-        )}
-      </Box>
-      <UserInfoItem
-        icon="ri-calendar-line"
-        label="تاریخ شروع قرارداد"
-        value={details ? convertUnixToJalali(details.contract_end) : "نامشخص"}
-      />
-      <UserInfoItem
-        icon="ri-calendar-line"
-        label="تاریخ اجرای قرارداد"
-        value={details ? convertUnixToJalali(details.execute_start) : "نامشخص"}
-      />
-      <UserInfoItem
-        icon="ri-wallet-2-line"
-        label="مبلغ حکم کارگزینی"
-        value="-"
-      />
-
+      {renderFields()}
       {rejectApprovalLevel > 0 && !readOnly && (
         <TextField
           fullWidth
