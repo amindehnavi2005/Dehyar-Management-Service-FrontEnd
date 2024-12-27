e074bad4ddd69f45ded6c8b1439a80c0f3e903f8
amin dehnavi <amin1384dehnavi@gmail.com>
2024-12-27 23:56:35 +0330

    add lazy-load to rebuildVillageRank

--- src/utils/rebuildVillageRank.js
+++ src/utils/rebuildVillageRank.js
@@ -1,26 +1,97 @@
+import ContentLoader from "react-content-loader";
+
 export const rebuildVillageRanks = (details) => {
+  const isLoading = !details || Object.keys(details).length === 0;
+  console.log("Details => ", details);
+  console.log("Is Loading => ", isLoading);
+
   return [
     {
       id: 1,
       parameter: "جمعیت",
-      year: (details?.populations && details?.populations[0]?.year) || "-",
+      year: isLoading ? (
+        <ContentLoader
+          speed={2}
+          width={100}
+          height={20}
+          viewBox="0 0 100 20"
+          backgroundColor="gray"
+          foregroundColor="#ecebeb"
+        >
+          <rect x="0" y="0" rx="4" ry="4" width="100" height="20" />
+        </ContentLoader>
+      ) : (
+        (details?.populations && details?.populations[0]?.year) || "-"
+      ),
       value: (details?.populations && details?.populations[0]?.population) || 0,
-      score: details?.population_score || 0,
+      score: isLoading ? (
+        <ContentLoader
+          speed={2}
+          width={100}
+          height={20}
+          viewBox="0 0 100 20"
+          backgroundColor="gray"
+          foregroundColor="#ecebeb"
+        >
+          <rect x="0" y="0" rx="4" ry="4" width="100" height="20" />
+        </ContentLoader>
+      ) : (
+        details?.population_score || 0
+      ),
     },
     {
       id: 2,
       parameter: "وسعت (هکتار)",
       year: "-",
       value: details?.area_hectares || 0,
-      score: details?.area_hectar_score || 0,
+      score: isLoading ? (
+        <ContentLoader
+          speed={2}
+          width={100}
+          height={20}
+          viewBox="0 0 100 20"
+          backgroundColor="gray"
+          foregroundColor="#ecebeb"
+        >
+          <rect x="0" y="0" rx="4" ry="4" width="100" height="20" />
+        </ContentLoader>
+      ) : (
+        details?.area_hectar_score || 0
+      ),
       isValueEditing: false,
     },
     {
       id: 3,
       parameter: "درآمد (میلیون ریال)",
-      year: details?.incomes?.length || "-",
+      year: isLoading ? (
+        <ContentLoader
+          speed={2}
+          width={40}
+          height={20}
+          viewBox="0 0 40 20"
+          backgroundColor="gray"
+          foregroundColor="#ecebeb"
+        >
+          <rect x="0" y="0" rx="4" ry="4" width="40" height="20" />
+        </ContentLoader>
+      ) : (
+        details?.incomes?.length || "-"
+      ),
       value: details?.incomes?.length || "-",
-      score: details?.income_per_capita_score || 0,
+      score: isLoading ? (
+        <ContentLoader
+          speed={2}
+          width={40}
+          height={20}
+          viewBox="0 0 40 20"
+          backgroundColor="gray"
+          foregroundColor="#ecebeb"
+        >
+          <rect x="0" y="0" rx="4" ry="4" width="40" height="20" />
+        </ContentLoader>
+      ) : (
+        details?.income_per_capita_score || 0
+      ),
       isYearEditing: false,
       isValueEditing: false,
     },
@@ -29,14 +100,40 @@ export const rebuildVillageRanks = (details) => {
       parameter: "هدف گردشگری",
       year: "-",
       value: details?.tourism_goal === 1,
-      score: details?.tourism_goal_score || 0,
+      score: isLoading ? (
+        <ContentLoader
+          speed={2}
+          width={100}
+          height={20}
+          viewBox="0 0 100 20"
+          backgroundColor="gray"
+          foregroundColor="#ecebeb"
+        >
+          <rect x="0" y="0" rx="4" ry="4" width="100" height="20" />
+        </ContentLoader>
+      ) : (
+        details?.tourism_goal_score || 0
+      ),
     },
     {
       id: 5,
       parameter: "مرکز دهستان",
       year: "-",
       value: details?.centralization === 1,
-      score: details?.centralization_score || 0,
+      score: isLoading ? (
+        <ContentLoader
+          speed={2}
+          width={100}
+          height={20}
+          viewBox="0 0 100 20"
+          backgroundColor="gray"
+          foregroundColor="#ecebeb"
+        >
+          <rect x="0" y="0" rx="4" ry="4" width="100" height="20" />
+        </ContentLoader>
+      ) : (
+        details?.centralization_score || 0
+      ),
     },
     {
       id: 6,
