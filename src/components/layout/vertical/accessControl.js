fb22176222df5542cb091c142321625d8655da57
amin dehnavi <amin1384dehnavi@gmail.com>
2024-12-28 11:14:01 +0330

    add badge to personelLists

--- src/components/layout/vertical/accessControl.js
+++ src/components/layout/vertical/accessControl.js
@@ -8,7 +8,7 @@ const accessControl = {
       label: "پرسنل دهیاری ها",
       icon: <i className="ri-survey-line" />,
       href: "/dehyari/rsd",
-      badge: "0",
+      badge: 0,
     },
     {
       label: "درجه بندی دهیاری",
@@ -108,7 +108,7 @@ const accessControl = {
       label: "پرسنل دهیاری ها",
       icon: <i className="ri-group-line" />,
       href: "/dehyari/governor/table",
-      badge: "0",
+      badge: 0,
     },
     {
       label: "مدیریت کاربران",
@@ -131,7 +131,7 @@ const accessControl = {
       label: "پرسنل دهیاری ها",
       icon: <i className="ri-group-line" />,
       href: "/dehyari/bakhshdar/table",
-      badge: "0",
+      badge: 0,
     },
     {
       label: "تشکیلات دهیاری",
