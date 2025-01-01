00690c0527b55e3e89973c0a6daae7f419ca5577
amin dehnavi <amin1384dehnavi@gmail.com>
2025-01-01 14:14:11 +0330

    add HistoryArrow to ReviewDecree

--- src/views/dehyari/form/workflow/ReviewDecree.jsx
+++ src/views/dehyari/form/workflow/ReviewDecree.jsx
@@ -8,6 +8,7 @@ import Chip from "@mui/material/Chip";
 import ArticleIcon from "@mui/icons-material/Article";
 import { Button, Typography } from "@mui/material";
 import CustomIconButton from "@/@core/components/mui/IconButton";
+import HistoryArrow from "@/components/badges/HistoryArrow";
 
 const ReviewDecree = ({
   details,
@@ -115,61 +116,11 @@ const ReviewDecree = ({
             label="نوع درخواست"
             value={details ? details.request_type : "نامشخص"}
           />
-          <Box
-            display="flex"
-            flexDirection="column"
-            alignItems="center"
-            gap={1}
-          >
-            <Typography
-              variant="caption"
-              sx={{
-                fontSize: "12px",
-                color: "text.secondary",
-                mb: -3,
-              }}
-            >
-              تاریخچه درجه‌بندی
-            </Typography>
-            <Box
-              display="flex"
-              alignItems="center"
-              justifyContent="center"
-              gap={2}
-              sx={{
-                position: "relative",
-                width: "100%",
-              }}
-            >
-              <div className="grid items-center rounded-full w-10 h-10 border text-center font-bold">
-                {details.current_grade}
-              </div>
-              <Box
-                sx={{
-                  height: "2px",
-                  width: "125px",
-                  background: "#ccc",
-                  position: "relative",
-                }}
-              >
-                <Box
-                  sx={{
-                    position: "absolute",
-                    top: "-4.5px",
-                    right: "-8px",
-                    width: "0",
-                    height: "0",
-                    borderLeft: "10px solid #ccc",
-                    borderTop: "6px solid transparent",
-                    borderBottom: "6px solid transparent",
-                  }}
-                />
-              </Box>
-              <div className="grid items-center rounded-full w-10 h-10 border text-center font-bold mr-2">
-                {details.new_grade}
-              </div>
-            </Box>
-          </Box>
+          <HistoryArrow
+            prevBadge={details?.current_grade}
+            nextBadge={details?.new_grade}
+            text={"تاریخچه درجه بندی"}
+          />
         </>
       );
     }
