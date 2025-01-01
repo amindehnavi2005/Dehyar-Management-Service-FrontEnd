04c0a11f4a229ffd812bed5aa40fd2069f6781b5
amin dehnavi <amin1384dehnavi@gmail.com>
2025-01-01 12:53:04 +0330

    update workFlowDrawerUpgradeVillageRank

--- src/views/dehyari/form/workflow/ReviewDecree.jsx
+++ src/views/dehyari/form/workflow/ReviewDecree.jsx
@@ -6,7 +6,8 @@ import { convertUnixToJalali } from "@/utils/dateConverter";
 import { downloadHumanResourcePdf } from "@/utils/humanResourcePdfUtils";
 import Chip from "@mui/material/Chip";
 import ArticleIcon from "@mui/icons-material/Article";
-import { Typography } from "@mui/material";
+import { Button, Typography } from "@mui/material";
+import CustomIconButton from "@/@core/components/mui/IconButton";
 
 const ReviewDecree = ({
   details,
@@ -114,24 +115,59 @@ const ReviewDecree = ({
             label="نوع درخواست"
             value={details ? details.request_type : "نامشخص"}
           />
-          <Box display="flex" flexDirection="column" alignItems="center">
+          <Box
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
+            gap={1}
+          >
             <Typography
               variant="caption"
               sx={{
                 fontSize: "12px",
                 color: "text.secondary",
-                marginBottom: "4px",
+                mb: -3,
               }}
             >
               تاریخچه درجه‌بندی
             </Typography>
-            <Box display="flex" alignItems="center" gap={1}>
-              <Typography variant="h4" color="error">
+            <Box
+              display="flex"
+              alignItems="center"
+              justifyContent="center"
+              gap={2}
+              sx={{
+                position: "relative",
+                width: "100%",
+              }}
+            >
+              <div className="grid items-center rounded-full w-10 h-10 border text-center font-bold">
                 {details.current_grade}
-              </Typography>
-              <Typography variant="h4" color="primary">
-                → {details.new_grade}
-              </Typography>
+              </div>
+              <Box
+                sx={{
+                  height: "2px",
+                  width: "125px",
+                  background: "#ccc",
+                  position: "relative",
+                }}
+              >
+                <Box
+                  sx={{
+                    position: "absolute",
+                    top: "-4px",
+                    right: "-10px",
+                    width: "0",
+                    height: "0",
+                    borderLeft: "10px solid #ccc",
+                    borderTop: "6px solid transparent",
+                    borderBottom: "6px solid transparent",
+                  }}
+                />
+              </Box>
+              <div className="grid items-center rounded-full w-10 h-10 border text-center font-bold mr-2">
+                {details.new_grade}
+              </div>
             </Box>
           </Box>
         </>
