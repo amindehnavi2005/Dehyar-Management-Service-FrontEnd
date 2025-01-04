e22c0f6aeb91dd963c118bcc30b68a8655d5471f
amin dehnavi <amin1384dehnavi@gmail.com>
2025-01-04 23:37:06 +0330

    add geoNames and try/catch to UpgradeVillageRank

--- src/views/dehyari/dehyar/upgrade-village-rank/UpgradeVillageRank.jsx
+++ src/views/dehyari/dehyar/upgrade-village-rank/UpgradeVillageRank.jsx
@@ -1,35 +1,91 @@
 "use client";
-import { Divider, Grid, Typography } from "@mui/material";
+import { Box, Divider, Grid, Typography } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import UpgradeVillageTable from "./list/UpgradeVillageTable";
 import UpgradeVillageInformation from "./list/UpgradeVillageInformation";
 import { getDivisionInformation } from "@/Services/UpgradeVillage";
 import { me } from "@/Services/Auth/AuthService";
 import api from "@/utils/axiosInstance";
+import UpgradeVillageLoading from "@/components/loadings/UpgradeVillageLoading";
+import { fetchGeoDetails } from "@/utils/fetchGeoDetails";
 
 const UpgradeVillageRank = () => {
   const [loading, setLoading] = useState(false);
   const [addEventSidebarOpen, setAddEventSidebarOpen] = useState(false);
-  const [divisionInformation, setDivisionInformation] = useState({});
-  const [userInfo, setUserInfo] = useState({});
+  const [divisionInformation, setDivisionInformation] = useState("");
+  const [geoNames, setGeoNames] = useState("");
+  const [userInfo, setUserInfo] = useState("");
 
   const handleAddEventSidebarToggle = () =>
     setAddEventSidebarOpen(!addEventSidebarOpen);
 
   useEffect(() => {
     const fetchData = async () => {
-      const response = await api.get(getDivisionInformation(), {
-        requiresAuth: true,
-      });
-      setDivisionInformation(response.data.data);
+      try {
+        const response = await api.get(getDivisionInformation(), {
+          requiresAuth: true,
+        });
+        setDivisionInformation(response.data.data);
+      } catch (error) {
+        console.error("Error fetching division information:", error);
+      } finally {
+        setLoading(false);
+      }
     };
+
     const userDetails = async () => {
-      const response = await api.get(`${me()}`, { requiresAuth: true });
-      setUserInfo(response.data.data.user.original);
+      try {
+        const response = await api.get(`${me()}`, { requiresAuth: true });
+        setUserInfo(response.data.data.user.original);
+      } catch (error) {
+        console.error("Error fetching user details:", error);
+      }
     };
+
     fetchData();
     userDetails();
   }, []);
+
+  useEffect(() => {
+    if (userInfo) {
+      const getGeoNames = async () => {
+        console.log("User Info => ", userInfo);
+        const geoState = userInfo?.geo_state;
+        const geoCity = userInfo?.geo_city;
+        const geoRegion = userInfo?.geo_region;
+        const geoDehestan = userInfo?.geo_dehestan;
+        const geoVillage = userInfo?.geo_village;
+        const geoData = await fetchGeoDetails({
+          geoState,
+          geoCity,
+          geoRegion,
+          geoDehestan,
+          geoVillage,
+        });
+        setGeoNames(geoData);
+      };
+
+      getGeoNames();
+    }
+  }, [userInfo]);
+
+  console.log("Geo Names => ", geoNames);
+  if (!divisionInformation || !geoNames) {
+    return (
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          width: "100%",
+          height: "100vh",
+          backgroundColor: "#f9f9f9",
+        }}
+      >
+        <UpgradeVillageLoading />
+      </Box>
+    );
+  }
 
   return (
     <Grid
@@ -44,6 +100,7 @@ const UpgradeVillageRank = () => {
           <UpgradeVillageInformation
             details={divisionInformation}
             userInfo={userInfo}
+            geoNames={geoNames}
           />
         </div>
         <Divider className="bg-backgroundDefault md:w-[3px] sm:w-full md:ml-5 mx-5" />
