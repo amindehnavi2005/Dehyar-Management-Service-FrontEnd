39f95bc827a029f0ca83a393a2e219e74cd5bcb5
amin dehnavi <amin1384dehnavi@gmail.com>
2025-01-04 23:36:00 +0330

    create new lazy-load

---
+++ src/components/loadings/UpgradeVillageLoading.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import ContentLoader from "react-content-loader";
+
+const UpgradeVillageLoading = () => {
+  return (
+    <div style={{ width: "100%", height: "100vh" }}>
+      <ContentLoader
+        speed={2}
+        width={"100%"}
+        height={"100%"}
+        viewBox="0 0 350 200"
+        backgroundColor="#e1e1e1"
+        foregroundColor="#ccc"
+      >
+        <rect x="189" y="9" rx="3" ry="3" width="93" height="7" />
+        <rect x="20" y="9" rx="2" ry="2" width="146" height="7" />
+        <rect x="190" y="83" rx="1" ry="1" width="93" height="10" />
+        <rect x="83" y="83" rx="1" ry="1" width="82" height="10" />
+        <rect x="263" y="34" rx="1" ry="1" width="16" height="4" />
+        <rect x="250" y="41" rx="1" ry="1" width="30" height="4" />
+        <rect x="250" y="56" rx="1" ry="1" width="30" height="4" />
+        <rect x="191" y="34" rx="2" ry="2" width="52" height="4" />
+        <rect x="122" y="25" rx="1" ry="1" width="29" height="4" />
+        <rect x="94" y="25" rx="1" ry="1" width="22" height="4" />
+        <rect x="60" y="25" rx="1" ry="1" width="27" height="4" />
+        <rect x="21" y="25" rx="1" ry="1" width="33" height="4" />
+        <rect x="190" y="41" rx="2" ry="2" width="52" height="4" />
+        <rect x="190" y="47" rx="2" ry="2" width="52" height="4" />
+        <rect x="190" y="56" rx="2" ry="2" width="52" height="4" />
+        <rect x="221" y="23" rx="5" ry="4" width="33" height="7" />
+        <rect x="189" y="26" rx="2" ry="2" width="91" height="1" />
+        <rect x="6" y="-176" rx="2" ry="2" width="91" height="1" />
+        <rect x="263" y="34" rx="1" ry="1" width="16" height="4" />
+        <rect x="263" y="34" rx="1" ry="1" width="16" height="4" />
+        <rect x="222" y="63" rx="5" ry="4" width="33" height="7" />
+        <rect x="190" y="66" rx="2" ry="2" width="91" height="1" />
+        <rect x="263" y="49" rx="1" ry="1" width="16" height="4" />
+        <rect x="191" y="74" rx="2" ry="2" width="50" height="4" />
+        <rect x="252" y="75" rx="1" ry="1" width="28" height="4" />
+        <rect x="154" y="22" rx="6" ry="5" width="12" height="11" />
+        <rect x="122" y="39" rx="1" ry="1" width="29" height="4" />
+        <rect x="94" y="39" rx="1" ry="1" width="22" height="4" />
+        <rect x="60" y="39" rx="1" ry="1" width="27" height="4" />
+        <rect x="21" y="39" rx="1" ry="1" width="33" height="4" />
+        <rect x="154" y="36" rx="6" ry="5" width="12" height="11" />
+        <rect x="122" y="53" rx="1" ry="1" width="29" height="4" />
+        <rect x="93" y="53" rx="1" ry="1" width="22" height="4" />
+        <rect x="60" y="53" rx="1" ry="1" width="27" height="4" />
+        <rect x="21" y="53" rx="1" ry="1" width="33" height="4" />
+        <rect x="153" y="50" rx="6" ry="5" width="12" height="11" />
+        <rect x="122" y="68" rx="1" ry="1" width="29" height="4" />
+        <rect x="93" y="68" rx="1" ry="1" width="22" height="4" />
+        <rect x="60" y="68" rx="1" ry="1" width="27" height="4" />
+        <rect x="52" y="85" rx="1" ry="1" width="27" height="4" />
+        <rect x="22" y="86" rx="1" ry="1" width="27" height="4" />
+        <rect x="21" y="68" rx="1" ry="1" width="33" height="4" />
+        <rect x="153" y="65" rx="6" ry="5" width="12" height="11" />
+      </ContentLoader>
+    </div>
+  );
+};
+
+export default UpgradeVillageLoading;
